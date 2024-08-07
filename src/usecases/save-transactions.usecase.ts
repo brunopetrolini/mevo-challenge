9aa1f292630f12e77ad0bab94a9c9336a94fddb7
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-07 13:58:26 -0300

    feat: find duplicated transactions

--- src/usecases/save-transactions.usecase.ts
+++ src/usecases/save-transactions.usecase.ts
@@ -42,31 +42,62 @@ export class SaveTransactionsUsecase {
         suspiciousTransactions.length + negativedTransactions.length,
     };
 
-    const transactionsToCreate = transactionsWithValidAmount.map(
-      (transaction) => ({
-        id: Buffer.from(
-          `${transaction.file}#${transaction.from}#${transaction.to}#${transaction.amount}`,
-        ).toString('base64'),
-        to: transaction.to,
-        from: transaction.from,
-        amount: transaction.amount,
-        fileName: transaction.file,
-      }),
-    );
+    const { uniquesTransactions, duplicatedTransactions } =
+      this.findDuplicatedTransactions(transactionsWithValidAmount);
+
+    if (duplicatedTransactions.length !== 0) {
+      rejectedTransactionsResume.reason =
+        'File has duplicate transactions and with invalid amount';
+
+      const totalNonProcessed =
+        suspiciousTransactions.length +
+        negativedTransactions.length +
+        duplicatedTransactions.length;
+      rejectedTransactionsResume.totalNonProcessed = totalNonProcessed;
+    }
+
+    const transactionsToCreate = uniquesTransactions.map((transaction) => ({
+      id: Buffer.from(
+        `${transaction.file}#${transaction.from}#${transaction.to}#${transaction.amount}`,
+      ).toString('base64'),
+      to: transaction.to,
+      from: transaction.from,
+      amount: transaction.amount,
+      fileName: transaction.file,
+    }));
 
     const transactionsResumePromise =
       this.prismaService.transactionsResume.create({
         data: rejectedTransactionsResume,
       });
 
-    const saveTransactionsPromise = this.prismaService.transaction.createMany({
-      data: transactionsToCreate,
-    });
+    const saveTransactionsPromise =
+      this.prismaService.transaction.createManyAndReturn({
+        data: transactionsToCreate,
+      });
 
     await Promise.all([saveTransactionsPromise, transactionsResumePromise]);
 
     return {
       transactionsProcessed: transactions.length,
     };
+  }
+
+  private findDuplicatedTransactions(transactions: Transaction[]) {
+    const uniquesSet = new Set<string>();
+    const uniques: Transaction[] = [];
+    const duplicates: Transaction[] = [];
+
+    transactions.forEach((transaction) => {
+      const identifier = JSON.stringify(transaction);
+      if (uniquesSet.has(identifier)) {
+        duplicates.push(transaction);
+      } else {
+        uniquesSet.add(identifier);
+        uniques.push(transaction);
+      }
+    });
+
+    return { uniquesTransactions: uniques, duplicatedTransactions: duplicates };
   }
 }
