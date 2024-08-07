982ebf660fa5db47f2f7714b9d968891bc793687
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-08-06 22:04:12 -0300

    feat: adds prisma service on root module

--- src/controllers/transactions-file-upload.controller.ts
+++ src/controllers/transactions-file-upload.controller.ts
@@ -3,18 +3,25 @@ import { parseFile } from 'fast-csv';
 import { resolve } from 'path';
 import { PrismaService } from 'src/services/prisma.service';
 
+type Transactions = {
+  to: string;
+  from: string;
+  amount: number;
+};
+
 @Controller('/transactions')
 export class TransactionsFileUploadController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Post('/upload')
   public async uploadFile() {
+    const parsedTransactions: Transactions[] = [];
+
     parseFile(resolve('./payload.csv'), {
       delimiter: ';',
       headers: true,
     })
       .on('error', (error) => console.error(error))
-      .on('data', (row) => console.log(row))
-      .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+      .on('data', (row) => parsedTransactions.push(row));
   }
 }
